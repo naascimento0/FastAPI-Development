5d559f2029e81404c2de6824213af75e40447645
naascimento0 <gabrieloliveiran@hotmail.com>
2024-05-22 10:16:04 -0300

    storing posts in array

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ from fastapi import FastAPI
 from fastapi.params import Body
 from pydantic import BaseModel
 from typing import Optional
+from random import randrange
 
 app = FastAPI()
 
@@ -12,16 +13,19 @@ class Post(BaseModel): # Post extends BaseModel
     published: bool = True # initialized variable
     rating: Optional[int] = None # initiliazed as none value
 
+my_posts = [{"title": "title of post 1", "content": "content of post 1", "id": 1}, {"title": "favorite_foods", "content": "I like pizza", "id": 2}]
+
 @app.get("/") # URL path
 def read_root():
     return {"Welcome to my API!"}
 
 @app.get("/posts")
 def get_posts():
-    return {"data": "This is your posts"}
+    return {"data": my_posts}
 
-@app.post("/createposts")
+@app.post("/posts")
 def create_posts(post: Post): # extract all of the fields from the body and convert to a python dictionary inside the varaible payLoad
-    print(post)
-    print(post.model_dump()) # convert to a dictionary
-    return {"data": post}
+    post_dict = post.model_dump()
+    post_dict['id'] = randrange(0, 1000000)
+    my_posts.append(post_dict)
+    return {"data": post_dict}
