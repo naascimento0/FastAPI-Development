8b0b80676c281de2fbd203e25cf556c744dc8d00
naascimento0 <gabrieloliveiran@hotmail.com>
2024-05-22 09:48:59 -0300

    validating user input with pydantic

--- main.py
+++ main.py
@@ -1,7 +1,16 @@
 from fastapi import FastAPI
 from fastapi.params import Body
+from pydantic import BaseModel
+from typing import Optional
 
 app = FastAPI()
+
+# fastAPI is going to automatically validate the data based on the model of this class
+class Post(BaseModel): # Post extends BaseModel
+    title: str
+    content: str
+    published: bool = True # initialized variable
+    rating: Optional[int] = None # initiliazed as none value
 
 @app.get("/") # URL path
 def read_root():
@@ -12,6 +21,7 @@ def get_posts():
     return {"data": "This is your posts"}
 
 @app.post("/createposts")
-def create_posts(payLoad: dict = Body(...)): # extract all of the fields from the body and convert to a python dictionary inside the varaible payLoad
-    print(payLoad)
-    return {"new_post": f"title: {payLoad['title']} content: {payLoad['content']}"}
+def create_posts(post: Post): # extract all of the fields from the body and convert to a python dictionary inside the varaible payLoad
+    print(post)
+    print(post.model_dump()) # convert to a dictionary
+    return {"data": post}
