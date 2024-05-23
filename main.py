45fe5ceef13c9fbe400b2317115db56284da7f33
naascimento0 <gabrieloliveiran@hotmail.com>
2024-05-23 11:19:07 -0300

    delete, excpetion, response, status

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, Response, status, HTTPException
 from fastapi.params import Body
 from pydantic import BaseModel
 from typing import Optional
@@ -15,6 +15,16 @@ class Post(BaseModel): # Post extends BaseModel
 
 my_posts = [{"title": "title of post 1", "content": "content of post 1", "id": 1}, {"title": "favorite_foods", "content": "I like pizza", "id": 2}]
 
+def find_post(id):
+    for p in my_posts:
+        if p['id'] == id:
+            return p
+        
+def find_index_post(id):
+    for i, p in enumerate(my_posts):
+        if p['id'] == id:
+            return i
+
 @app.get("/") # URL path
 def read_root():
     return {"Welcome to my API!"}
@@ -23,9 +33,33 @@ def read_root():
 def get_posts():
     return {"data": my_posts}
 
-@app.post("/posts")
+@app.post("/posts", status_code = status.HTTP_201_CREATED)
 def create_posts(post: Post): # extract all of the fields from the body and convert to a python dictionary inside the varaible payLoad
     post_dict = post.model_dump()
     post_dict['id'] = randrange(0, 1000000)
     my_posts.append(post_dict)
     return {"data": post_dict}
+
+@app.get("/posts/latest")
+def get_latest_post():
+    post = my_posts[len(my_posts) - 1]
+    return {"detail": post}
+
+@app.get("/posts/{id}")
+def get_post(id: int, response: Response):
+    post = find_post(id)
+    if not post:
+        raise HTTPException(status_code = status.HTTP_404_NOT_FOUND,
+                             detail = f"post with id: {id} was not found")
+        # response.status_code = status.HTTP_404_NOT_FOUND
+        # return {'message': f"post with id: {id} was not found"}
+    return {"post_detail": post}
+
+@app.delete("/posts/{id}", status_code = status.HTTP_204_NO_CONTENT)
+def delete_post(id: int):
+    index = find_index_post(id)
+    if index == None:
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, 
+                            detail = f"post with id: {id} does not exist")
+    my_posts.pop(index)
+    return Response(status_code = status.HTTP_204_NO_CONTENT)
