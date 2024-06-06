862cbd7376de548e573e34734aa28fafb7a896c1
naascimento0 <gabrieloliveiran@hotmail.com>
2024-06-06 09:09:36 -0300

    setting CORS, adding README and pip install requirements

---
+++ README.md
@@ -0,0 +1,3 @@
+pip install -r requirements.txt
+
+fastapi dev app/main.py
--- app/main.py
+++ app/main.py
@@ -2,10 +2,20 @@ from fastapi import FastAPI
 from . import models
 from .database import engine
 from .routers import post, user, auth, vote
+from fastapi.middleware.cors import CORSMiddleware
 
 # models.Base.metadata.create_all(bind=engine) # alembic made this unnecessary
 
 app = FastAPI()
+
+origins = ["*"]
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins, # domains that talk to the api
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
         
 app.include_router(post.router)
 app.include_router(user.router)
